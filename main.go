6031730d008d5085095752faba532e11221bc439
Houcinee <lahoucinegahboub@gmail.com>
2026-02-21 07:41:53 +0000

    ADD PID Namespace Isolation and fix the ps problem

--- main.go
+++ main.go
@@ -8,18 +8,47 @@ import (
 )
 
 func main() {
-	cmd := exec.Command("bash")
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("Bad usage")
+	}
+}
+
+func parent() {
+	arguments := append([]string{"child"}, os.Args[2:]...)
+
+	cmd := exec.Command("/proc/self/exe", arguments...)
 
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
-	cmd.SysProcAttr = &syscall.SysProcAttr{Cloneflags: syscall.CLONE_NEWUTS}
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	}
 
-	fmt.Println("We are doing our thing")
+	err := cmd.Run()
+	handle(err)
+}
 
-	if err := cmd.Run(); err != nil {
-		fmt.Println("error :", err)
-		return
+func child() {
+	handle(syscall.Mount("", "/", "", syscall.MS_REC|syscall.MS_PRIVATE, "")) // Mount and unmount will not propagate to parent
+	handle(syscall.Mount("proc", "/proc", "proc", 0, ""))
+
+	binary, err := exec.LookPath(os.Args[2]) // syscall.Exec() requires a full path to the binary
+	handle(err)
+
+	args := os.Args[3:]
+
+	handle(syscall.Exec(binary, args, os.Environ()))
+}
+
+func handle(err error) {
+	if err != nil {
+		fmt.Println("error : ", err)
 	}
 }
